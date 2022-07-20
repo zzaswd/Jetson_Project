1377b5d2b241a9cc3e8b21a4d0a6bafd74241fbc
zzaswd <78605108+zzaswd@users.noreply.github.com>
2022-07-20 22:50:45 +0900

    22.07.20 교수님 피드백 후 전면 수정. 차단기 역할 추가 및 Maria DB 추가, 입구에서 차량 번호판 인식 후 자리 안내하며 차량번호로 충전구 모양 파악

---
+++ Change_Format/220720_Raspberry_Blocking_Car_Success/ksh_rasp/workspace/build/CMakeFiles/Capture.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Capture.dir/Capture.cpp.o"
+  "Capture"
+  "Capture.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Capture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
